3ff823a1aa058cd25dfe62a27ada20fd88298ac5
sejin\admin <seijin0722@naver.com>
2022-08-27 02:35:12 +0900

    1. SecurityConfig 클래스 (TokenProvider와 JwtFilter를 SecurityConfig에 적용할때 사용할 JwtSecurityConfig클래스) 2. 유효한 자격증명인지, 필요권한없는지 확인하는 접근제한 클래스 3. 그동안 만든 jwt를 적용할 SecurityConfig 클래스

--- jwt-tutorial/src/main/java/me/silvernine/tutorial/config/SecurityConfig.java
+++ jwt-tutorial/src/main/java/me/silvernine/tutorial/config/SecurityConfig.java
@@ -1,29 +1,86 @@
 package me.silvernine.tutorial.config;
 
+import me.silvernine.tutorial.jwt.JwtSecurityConfig;
+import me.silvernine.tutorial.jwt.JwtAccessDeniedHandler;
+import me.silvernine.tutorial.jwt.JwtAuthenticationEntryPoint;
+import me.silvernine.tutorial.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(prePostEnabled = true)//나중애 @PreAuthorize를 메소드단위로 추가하기 위해 사용
+public class SecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    @Override
-    public void configure(WebSecurity web) {
-        web
-                .ignoring()
-                .antMatchers(
-                        "/h2-console/**",
-                        "/favicon.ico"
-                );
+    //기존에 만들었던 jwt관련 클래스를 주입받는다
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/h2-console/**"
+                , "/favicon.ico"
+                , "/error");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf().disable()
+                //exceptionHandling할때 우리가 만들었던 클래스 추가
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // enable h2-console : h2-console을 위한 설정 추가
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
                 .authorizeRequests()
                 .antMatchers("/api/hello").permitAll()
-                .anyRequest().authenticated();
-    }
+                //토큰을 받기위한 로그인 API인 /api/authenticate와
+                //회원가인 API인 /api/signup는 토큰이 없는 상태에서 요청이 들어오기 때문에
+                //모두 permitAll()해준다
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
 
+                .anyRequest().authenticated()
+
+                .and()
+                //JwtFilter를 addFilterBefore로 등록했던
+                //JwtSecurityConfig클래스도 적용
+                .apply(new JwtSecurityConfig(tokenProvider));
+
+        return httpSecurity.build();
+    }
 }
---
+++ jwt-tutorial/src/main/java/me/silvernine/tutorial/jwt/JwtSecurityConfig.java
@@ -0,0 +1,24 @@
+package me.silvernine.tutorial.jwt;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+//SecurityConfigurerAdapter를 extends하고 TokenProvider를 주입받아서
+//18번라인 configure메소드를  @Override해서 20번라인 JwtFilter를 시큐리티 로직에 등록하는 과정
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private TokenProvider tokenProvider;
+    public JwtSecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) {
+        http.addFilterBefore(
+                new JwtFilter(tokenProvider),
+                UsernamePasswordAuthenticationFilter.class
+        );
+    }
+}
